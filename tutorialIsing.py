7ab60d5019272c73743fe691efcd10fc6ae04df7
baaelbre <44871173+baaelbre@users.noreply.github.com>
2020-10-23 21:21:52 +0200

    tutorialIsing-magnetization curve

--- tutorialIsing.py
+++ tutorialIsing.py
@@ -1,21 +1,16 @@
 from tutorialFunctions import *
 import matplotlib.pyplot as plt
 from time import time
-'''
-def Z(beta, J, Ac, Fl, Fr):
-    TN = ncon((Fl, Ac, O_tensor(beta, J), np.conj(Ac), Fr), ([1, 3, 2], [2,7,5],[3,7,8,6],[1,6,4], [5,8,4]))
-    return TN
-'''
+
 D = 12
 d = 2
-A = createMPS(D,d)
-Al, Ar, Ac, C = mixedCanonical(A)
 J=1
 
+print('Bond dimension: D =', D)
+A = createMPS(D, d)
+Al, Ar, Ac, C = mixedCanonical(A)
 # optimization parameters
-delta = 1e-4
-tol = 1e-3
-flag = 1
+tol = 1e-5
 
 T_array = np.linspace(0.2,3,20)
 magnetizations = []
@@ -23,6 +18,9 @@ magnetizations_exact = []
 for T in T_array:
     beta = 1/T
     O = isingO(beta, J)
+    t0 = time()
+    delta = 1e-4
+    flag = 1
     while flag:
         lam, Fl = leftFixedPointMPO(Al, O, delta)
         _ , Fr = rightFixedPointMPO(Ar, O, delta)
@@ -32,22 +30,25 @@ for T in T_array:
         AlPrime, ArPrime, AcPrime, cPrime = minAcC(AcPrime, cPrime)
         delta = np.linalg.norm(OAc(Ac, Fl, Fr, O, lam) - ncon((Al, OC(C, Fl, Fr)), ([-1, -2, 1], [1, -3])))
         Al, Ar, Ac, C = AlPrime, ArPrime, AcPrime, cPrime
+        print(delta)
         i += 1
         if delta < tol:
             flag = 0
+            
     print('##############')
     print('T={}'.format(T))
     print('Time for VUMPS optimization:', time()-t0, 's')
     print('Iterations needed:', i)
     freeEnergy = freeEnergyDensity(beta, lam)
-    freeEnergyExact = isingExact(beta, 1)[2]
+    freeEnergyExact = isingExact(beta, J)[1]
     print('Computed free energy:', freeEnergy)
     print('Exact free energy:', freeEnergyExact)
     print('##############')
 
     magnetizations.append(isingMagnetization(beta, J, Ac, Fl, Fr)/isingZ(beta, J, Ac, Fl, Fr))
     magnetizations_exact.append(isingExact(beta, J)[0])
-    
+
+plt.figure()
 plt.xlabel(r'$T$')
 plt.ylabel(r'$<M>$')
 plt.plot([T for T in T_array], magnetizations, label = 'D={}'.format(D))
